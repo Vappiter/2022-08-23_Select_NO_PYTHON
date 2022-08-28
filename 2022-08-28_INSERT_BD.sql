070d6fc9227259f6ab69cf73942e7672309672dd
Andrey Vladimirov <vappiter@yandex.ru>
2022-08-29 00:04:42 +0300

    WORK is BigComp

---
+++ 2022-08-28_INSERT_BD.sql
@@ -0,0 +1,22 @@
+/* Добавляем жанры */
+insert into genre(genre_name) values ('rok');
+delete from genre where genre_name = 'rok';
+insert into genre(genre_name) values ('roсk');
+insert into genre(genre_name) values ('punk rock');
+insert into genre(genre_name) values ('jazz');
+insert into genre(genre_name) values ('opera');
+insert into genre(genre_name) values ('pop');
+
+/* Добавляем исполнителей */
+
+insert into musician(musician_name, musician_nickname) values ('ДДТ', 'Шевчук');
+insert into musician(musician_name, musician_nickname) values ('Корлль и шут', 'Горшок');
+insert into musician(musician_name, musician_nickname) values ('Чайф', 'Екатеринбург');
+insert into musician(musician_name, musician_nickname) values ('QUEEN', 'Freddie Mercury');
+insert into musician(musician_name, musician_nickname) values ('Хор Турецкого', '12 мужчин');
+insert into musician(musician_name, musician_nickname) values ('Arabesques', 'Sandra');
+insert into musician(musician_name, musician_nickname) values ('Monserat Caballe', 'soprano');
+insert into musician(musician_name, musician_nickname) values ('Игорь Бутман', 'Saxophone');
+
+/* Добавляем альбомы */ 
+
